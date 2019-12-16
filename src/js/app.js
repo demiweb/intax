4e7676287e58c64058f44b8946effbd634b9b867
yurayarosh <yura12323@gmail.com>
2019-12-16 13:31:07 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import 'core-js/features/symbol'
+import 'core-js/features/array/from'
+import 'core-js/features/promise'
+import 'core-js/features/object/assign'
+import 'intersection-observer'
+import './lib/polyfill'
+
+import sayHello from './lib/sayHello'
+import setHTMLClassNames from './components/setHTMLClassNames'
+import setLazy from './components/setLazy'
+
+document.addEventListener('DOMContentLoaded', () => {
+  sayHello()
+  setHTMLClassNames()
+  setLazy()
+})
